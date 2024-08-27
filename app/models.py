00d0761fe5aadf995570581c75d3825825df8b9f
jhony-22 <jhonnyzaraza178@gmail.com>
2024-08-27 11:09:35 -0500

    comentarios

--- app/models.py
+++ app/models.py
@@ -22,29 +22,14 @@ class Marca(db.Model):
     modelo = db.Column(db.String(45))
     serie = db.Column(db.String(45))
 
-class Factura(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    fecha = db.Column(db.Date)
-    total = db.Column(db.Integer)
+class Comentarios(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comentario = db.Column(db.Text, nullable=False)
     idUsuarios = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+    idMotos = db.Column(db.Integer, db.ForeignKey('motos.id'), nullable=False)
 
-
-class DetalleFactura(db.Model):
-    idfactura = db.Column(db.Integer, db.ForeignKey('factura.id'), primary_key=True, nullable=False)
-    cantida = db.Column(db.Integer)
-    precioU = db.Column(db.Integer)
-    subtotal = db.Column(db.Integer)
-    nombreM = db.Column(db.String(45))
-    idMotos = db.Column(db.Integer, db.ForeignKey('motos.id'), primary_key=True, nullable=False)
-
-class Carito(db.Model):
-    idUsuarios = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key=True, nullable=False)
-    nombreM = db.Column(db.String(45))
-    cantidad = db.Column(db.Integer)
-    precioU = db.Column(db.Integer)
-    subtotal = db.Column(db.Integer)
-    foto = db.Column(db.String(200))
-    idMotos = db.Column(db.Integer, db.ForeignKey('motos.id'), primary_key=True, nullable=False)
+    usuarios = db.relationship('Usuarios', backref=db.backref('comentario', lazy=True))
+    motos = db.relationship('Motos', backref=db.backref('comentario', lazy=True))
 
 
  
