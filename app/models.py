85d006c6b3604bfec5c424ad3cec0c39e212a999
jhony-22 <jhonnyzaraza178@gmail.com>
2024-08-28 11:03:50 -0500

    registro de motos por la session

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ class Usuarios(db.Model):
     correo = db.Column(db.String(200))
     clave = db.Column(db.String(200))
     telefono = db.Column(db.BigInteger)
+    
+    motos = db.relationship('Motos', backref=db.backref('usuarios', lazy=True))
 
 class Motos(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -14,8 +16,12 @@ class Motos(db.Model):
     cantidad = db.Column(db.Integer)
     precio = db.Column(db.Integer)
     foto = db.Column(db.String(200))
-    marca_id = db.Column(db.Integer, db.ForeignKey('marca.id'), nullable=False)
-
+    descripcion = db.Column(db.Text)
+    marca_id = db.Column(db.Integer, db.ForeignKey('marca.id'), nullable=False)  
+    usuarios_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False) 
+    
+    marca = db.relationship('Marca', backref='motos')
+    
 class Marca(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(45))
--- mundosbk(1).sql
+++ mundosbk(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-08-2024 a las 22:12:27
+-- Tiempo de generación: 28-08-2024 a las 17:59:07
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,46 +24,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `carito`
+-- Estructura de tabla para la tabla `comentarios`
 --
 
-CREATE TABLE `carito` (
-  `idUsuarios` int(11) NOT NULL,
-  `nombreM` varchar(45) DEFAULT NULL,
-  `cantidad` int(11) DEFAULT NULL,
-  `precioU` int(11) DEFAULT NULL,
-  `subtotal` int(11) DEFAULT NULL,
-  `foto` varchar(200) DEFAULT NULL,
-  `idMotos` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `detalle_factura`
---
-
-CREATE TABLE `detalle_factura` (
-  `idfactura` int(11) NOT NULL,
-  `cantida` int(11) DEFAULT NULL,
-  `precioU` int(11) DEFAULT NULL,
-  `subtotal` int(11) DEFAULT NULL,
-  `nombreM` varchar(45) DEFAULT NULL,
-  `idMotos` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `factura`
---
-
-CREATE TABLE `factura` (
+CREATE TABLE `comentarios` (
   `id` int(11) NOT NULL,
-  `fecha` date DEFAULT NULL,
-  `total` int(11) DEFAULT NULL,
-  `idUsuarios` int(11) NOT NULL
+  `idUsuarios` int(11) NOT NULL,
+  `idMotos` int(11) NOT NULL,
+  `comentario` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `idUsuarios`, `idMotos`, `comentario`) VALUES
+(4, 1, 8, 'muy buena moto \r\n');
 
 -- --------------------------------------------------------
 
@@ -75,8 +51,21 @@ CREATE TABLE `marca` (
   `id` int(11) NOT NULL,
   `nombre` varchar(45) DEFAULT NULL,
   `modelo` varchar(45) DEFAULT NULL,
-  `serie` varchar(45) DEFAULT NULL
+  `serie` varchar(45) DEFAULT NULL,
+  `logo` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `marca`
+--
+
+INSERT INTO `marca` (`id`, `nombre`, `modelo`, `serie`, `logo`) VALUES
+(1, 'YAMAHA', '2015-2024', 'yzf-r', NULL),
+(2, 'HONDA', '2015-2024', 'cbr', NULL),
+(3, 'SUSUKY', '2015-2020', 'gsxr', NULL),
+(4, 'KAWASAKY', '2015-2024', 'ZX-RR', NULL),
+(5, 'BMW', '2015-2024', 'S-RR', NULL),
+(6, 'DUCATI', '2015-2024', 'panigale', NULL);
 
 -- --------------------------------------------------------
 
@@ -90,8 +79,19 @@ CREATE TABLE `motos` (
   `cantidad` int(11) DEFAULT NULL,
   `precio` int(11) DEFAULT NULL,
   `foto` varchar(200) DEFAULT NULL,
-  `marca_id` int(11) NOT NULL
+  `descripcion` text DEFAULT NULL,
+  `marca_id` int(11) NOT NULL,
+  `usuarios_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `motos`
+--
+
+INSERT INTO `motos` (`id`, `nombre`, `cantidad`, `precio`, `foto`, `descripcion`, `marca_id`, `usuarios_id`) VALUES
+(8, 'GSX-S 750', 30, 55000000, 'xayA7v8C.jpg', 'La Suzuki GSX-S750 es una motocicleta naked deportiva que combina un diseño agresivo con un rendimiento excepcional. Equipada con un motor de 749 cc, ofrece una potencia suave y controlable, ideal tanto para el uso en ciudad como para rutas más largas. Su chasis compacto y ágil proporciona una experiencia de conducción dinámica, mientras que su estética moderna y elegante capta la atención al instante.', 3, 1),
+(9, 'M 1000R', 17, 200000000, 'gsouYktl.jpg', 'La BMW M 1000 R es una motocicleta naked de alto rendimiento que lleva la emoción de la pista a las calles. Con un motor de 999 cc derivado de la superbike M 1000 RR, ofrece una potencia impresionante y una aceleración explosiva. Su chasis y suspensión ajustados para la máxima precisión aseguran un manejo ágil y preciso. Además, cuenta con tecnología avanzada y un diseño aerodinámico que refuerza su carácter deportivo y agresivo, haciendo de la M 1000 R una máquina diseñada para los entusiastas más exigentes.', 5, 1),
+(11, 'ZX10RR', 30, 120000000, 'F0lMKR4K.jpg', 'La Kawasaki ZX-10RR es una versión de alto rendimiento de la ZX-10R, diseñada específicamente para la competición. Equipada con un motor de 998 cc, entrega una potencia impresionante con una configuración optimizada para la pista. Destaca por su chasis ágil, suspensión avanzada y frenos de alta precisión, todo orientado a ofrecer la máxima velocidad y control en circuitos. Su diseño aerodinámico y acabados exclusivos la convierten en una de las motos más deseadas para los entusiastas de la velocidad y la competición.', 4, 4);
 
 -- --------------------------------------------------------
 
@@ -112,36 +112,22 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `clave`, `telefono`) VALUES
-(1, 'jhonny', 'jhonny@com.co', '1234', 232142),
-(2, 'yu', 'yu@com.co', 'pbkdf2:sha256:600000$uvpTusUk4ydgiumi$bab46d0eda7db4581b90f6d3cce136b1bd96581a6bf7fea9e00dbe7665541609', 3213345445),
-(3, 'yuky', 'yuky@com.co', 'scrypt:32768:8:1$8vGGixH88SHZ6frA$dcdced98f0ea8307d20679ddc8f3127f315f8a982e69d7ecba99eb24ff074fc1589d4e8e9ecd326884557eaa0dfc47ef6ab2e52dcd90806a69ffc392da18a1a3', 3213345445);
+(1, 'yuky', 'yuky@com.co', 'scrypt:32768:8:1$Z5ycmpwL268WTfOw$5ab56b102b14eb4a90cc1ad6477f6b846e57ea3e71c3dc6ff84ad0d7acff94bed0159f16a0c9062034ac1243006db3e0ad227c2b3d28a7a4a48fbd4977f2b32f', 3213345445),
+(2, 'scar', 'scar@con', 'scrypt:32768:8:1$VlvzeCxxNdwSFeBg$c50a2ac275cfba26e99cdff0337c1fdb8077cb49d40581b2f8c5936cdd311eea05d7d12df2f3ad1e9dc443f84d742a5deb25234da8c0bbfc87a9368dd6e2d9ef', 3213345445),
+(3, 'juan', 'juan@com.co', 'scrypt:32768:8:1$AUyyRWVfr8CpkRJh$986f50a71a77a2e1ca59193abdff05a36d2e9459a3188638f9f63d0065f3afde9f24902828c8f4d49667356ca5d23d046076df1d57c7e5f44f5e3102580186e0', 3213386883),
+(4, 'jhonny', 'jhonny@com.co', 'scrypt:32768:8:1$DAbFA1qTT9D5VN7N$8c67435aa3aae2eb4928b153a4ab4bceb6e0c7d8757123484f6f5afdf387d8c08a34f3b0b9900971f0b4fc51cabc7a937231837df49a7325e7d7f409ca3bd8a9', 3213345445);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `carito`
+-- Indices de la tabla `comentarios`
 --
-ALTER TABLE `carito`
-  ADD PRIMARY KEY (`idUsuarios`,`idMotos`),
-  ADD KEY `fk_repuestos_has_usuarios_usuarios1_idx` (`idUsuarios`),
-  ADD KEY `fk_carito_motos1_idx` (`idMotos`);
-
---
--- Indices de la tabla `detalle_factura`
---
-ALTER TABLE `detalle_factura`
-  ADD PRIMARY KEY (`idfactura`,`idMotos`),
-  ADD KEY `fk_factura_has_repuestos_factura1_idx` (`idfactura`),
-  ADD KEY `fk_detalle_factura_motos1_idx` (`idMotos`);
-
---
--- Indices de la tabla `factura`
---
-ALTER TABLE `factura`
-  ADD PRIMARY KEY (`id`,`idUsuarios`),
-  ADD KEY `fk_factura_usuarios_idx` (`idUsuarios`);
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`,`idUsuarios`,`idMotos`),
+  ADD KEY `fk_usuarios_has_motos_motos1_idx` (`idMotos`),
+  ADD KEY `fk_usuarios_has_motos_usuarios1_idx` (`idUsuarios`);
 
 --
 -- Indices de la tabla `marca`
@@ -153,8 +139,9 @@ ALTER TABLE `marca`
 -- Indices de la tabla `motos`
 --
 ALTER TABLE `motos`
-  ADD PRIMARY KEY (`id`,`marca_id`),
-  ADD KEY `fk_repuestos_marca1_idx` (`marca_id`);
+  ADD PRIMARY KEY (`id`,`marca_id`,`usuarios_id`),
+  ADD KEY `fk_repuestos_marca1_idx` (`marca_id`),
+  ADD KEY `fk_motos_usuarios1_idx` (`usuarios_id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -167,57 +154,45 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `factura`
+-- AUTO_INCREMENT de la tabla `comentarios`
 --
-ALTER TABLE `factura`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `marca`
 --
 ALTER TABLE `marca`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `motos`
 --
 ALTER TABLE `motos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `carito`
+-- Filtros para la tabla `comentarios`
 --
-ALTER TABLE `carito`
-  ADD CONSTRAINT `fk_carito_motos1` FOREIGN KEY (`idMotos`) REFERENCES `motos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_repuestos_has_usuarios_usuarios1` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `detalle_factura`
---
-ALTER TABLE `detalle_factura`
-  ADD CONSTRAINT `fk_detalle_factura_motos1` FOREIGN KEY (`idMotos`) REFERENCES `motos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_factura_has_repuestos_factura1` FOREIGN KEY (`idfactura`) REFERENCES `factura` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `factura`
---
-ALTER TABLE `factura`
-  ADD CONSTRAINT `fk_factura_usuarios` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `fk_usuarios_has_motos_motos1` FOREIGN KEY (`idMotos`) REFERENCES `motos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_usuarios_has_motos_usuarios1` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `motos`
 --
 ALTER TABLE `motos`
+  ADD CONSTRAINT `fk_motos_usuarios1` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_repuestos_marca1` FOREIGN KEY (`marca_id`) REFERENCES `marca` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
