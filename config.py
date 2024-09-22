2fb940ca56a923bc9fb95cc623fa67ff9111dd4b
yukuy <162237416+yukuy@users.noreply.github.com>
2024-09-21 20:11:17 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import secrets
 from datetime import timedelta
 
 class config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/railway'
-    #SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/railway'  # Conexión a MySQL
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/railway'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/railway'  # Conexión a MySQL
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desactivar rastreo de modificaciones para mejorar rendimiento
     SECRET_KEY = secrets.token_hex(16)   # Clave secreta para la app
     
